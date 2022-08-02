35c5b0b06cc5449e309916f201c28244ecefef49
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-08-02 13:18:19 -0600

    commit tree 0d3b1d5a5b40e7bdfda57222dd56ecb64e1eb604

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+{function submitData(name, email){
+    const userData = {
+        name: name,
+        email: email
+    }
+    let body = document.querySelector('body')
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    }
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(object) {
+            console.log(object.id)
+            body.append(object.id)
+        })
+        .catch(function(error) {
+            alert("THIS ISN'T GOOD!")
+            console.log(error.message)
+            body.append(error.message)
+        })
+}}
